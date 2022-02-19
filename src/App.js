782cb016d6c59f932c9337b2fe08a72a4411d0f2
tomohiko-f <tomofuji425@gmail.com>
2022-02-19 17:40:44 +0900

    fix: New post functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './About'
 import Missing from './Missing'
 import {Route, Routes, useNavigate} from 'react-router-dom'
 import {useState, useEffect} from 'react'
+import { format } from 'date-fns'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -38,12 +39,26 @@ function App() {
   ])
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp')
+    const newPost = {id, title: postTitle, datetime, body: postBody};
+    const allPosts = {...posts, newPost};
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/')
+  }
 
   const handleDelete = (id) => {
     const postsList = posts.filter(post => post.id !== id);
     setPosts(postsList);
-    navigate("/")
+    navigate('/')
   }
 
 
@@ -53,7 +68,13 @@ function App() {
       <Nav search={search} setSearch={setSearch} />
       <Routes>
         <Route path="/" element={<Home posts={posts} />} />
-        <Route path="/post" element={<NewPost />} />
+        <Route path="/post" element={<NewPost 
+          handleSubmit={handleSubmit}
+          postTitle={postTitle}
+          setPostTitle={setPostTitle}
+          postBody={postBody}
+          setPostBody={setPostBody}
+        />} />
         <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />} />
         <Route path="/about" element={<About />}/>
         <Route path="*" element={<Missing />}/>
