f9935c51c0916e617795efab0d20a1321a0f8823
tomohiko-f <tomofuji425@gmail.com>
2022-02-22 19:30:10 +0900

    fix: Edit post

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './Nav'
 import Footer from './Footer'
 import Home from './Home'
 import NewPost from './NewPost'
+import EditPost from './EditPost'
 import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
@@ -17,6 +18,8 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -46,22 +49,46 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp')
     const newPost = {id, title: postTitle, datetime, body: postBody};
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/')
+    try {
+      const response = await api.post('/posts', newPost)
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/')
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/')
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = {id, title: editTitle, datetime, body: editBody};
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? {...response.data} : post));
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`)
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/')
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
   }
 
 
@@ -78,7 +105,19 @@ function App() {
           postBody={postBody}
           setPostBody={setPostBody}
         />} />
-        <Route path="/post/:id" element={<PostPage posts={posts} handleDelete={handleDelete} />} />
+         <Route path="/edit/:id" element={<EditPost 
+          posts={posts}
+          handleEdit={handleEdit}
+          editTitle={editTitle}
+          setEditTitle={setEditTitle}
+          editBody={editBody}
+          setEditBody={setEditBody}
+        />} />
+        <Route path="/post/:id" element={<PostPage 
+          posts={posts} 
+          handleEdit={handleEdit} 
+          handleDelete={handleDelete} 
+        />} />
         <Route path="/about" element={<About />}/>
         <Route path="*" element={<Missing />}/>
       </Routes>
--- src/PostPage.js
+++ src/PostPage.js
@@ -1,6 +1,6 @@
 import { useParams, Link } from "react-router-dom"
 
-const PostPage = ({posts, handleDelete}) => {
+const PostPage = ({posts, handleEdit, handleDelete}) => {
   const {id} = useParams();
   const post = posts.find(post => (post.id).toString() === id);
     return (
@@ -11,7 +11,12 @@ const PostPage = ({posts, handleDelete}) => {
               <h2>{post.title}</h2>
               <p className="postDate">{post.datetime}</p>
               <p className="postBody">{post.body}</p>
-              <button onClick={() => handleDelete(post.id)}>
+              <Link to={`/edit/${post.id}`}>
+                <button className="editButton">
+                  Edit Post
+                </button>
+              </Link>
+              <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                 Delete Post
               </button>
             </>
